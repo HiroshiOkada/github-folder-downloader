fdc4475babdd3e3b13b2ec0e32cab794f78c056a
Hiroshi Okada <okadahiroshi@miobox.jp>
2025-02-08 17:25:01 +0900

    fix module structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="github-folder-downloader",
     version="0.1.0",
-    packages=find_packages(where="src"),
+    packages=["github_folder_downloader"],
     package_dir={"": "src"},
     entry_points={
         "console_scripts": [
