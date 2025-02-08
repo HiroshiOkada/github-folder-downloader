9ebbd6bf93c1c32a7c0510b109422e5d2e2a0a97
Hiroshi Okada <okadahiroshi@miobox.jp>
2025-02-08 17:36:54 +0900

    Update test_tool.sh to use URL with embedded branch and subfolder

--- test_tool.sh
+++ test_tool.sh
@@ -11,9 +11,8 @@ pip install -e . --break-system-packages
 echo "Cloning repository using git clone..."
 git clone git@github.com:githubtraining/hellogitworld.git "${TMP_DIR}/hellogitworld"
 
-echo "Using the CLI tool to download repository contents with branch master..."
-# Using --branch master because the repository uses master branch.
-github-folder-downloader https://github.com/githubtraining/hellogitworld.git "${TMP_DIR}/hellogitworld_download" --branch master
+echo "Using the CLI tool to download repository contents for subfolder 'src' on branch 'master'..."
+github-folder-downloader https://github.com/githubtraining/hellogitworld/tree/master/src "${TMP_DIR}/hellogitworld_download"
 
 echo "Comparing downloaded 'src' folder with the cloned repository 'src' folder..."
 # Use diff to compare the contents recursively.
